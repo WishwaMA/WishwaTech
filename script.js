5f335523f6b64b594e0ae289c9bd6f1649164e19
WishwaMA <154093373+WishwaMA@users.noreply.github.com>
2024-09-06 22:54:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+//back-end development
+
+//sinhala
+document.getElementById("sinhala").onclick = function(){
+    window.location.href = "sinhala.html";
+}
+
+//maths
+document.getElementById("maths").onclick = function() {
+    window.location.href = "maths.html";
+}
+
+//history
+document.getElementById("history").onclick = function() {
+    window.location.href = "history.html";
+}
+
+//science
+document.getElementById("sci").onclick = function() {
+    window.location.href = "science.html";
+}
+
+//english
+document.getElementById("en").onclick = function() {
+    window.location.href = "english.html";
+}
+
+//buddhist
+document.getElementById("bu").onclick = function() {
+    window.location.href = "buddhist.html";
+}
+
+//commerce
+document.getElementById("com").onclick = function() {
+    window.location.href = "commerce.html";
+}
+
+//civic
+document.getElementById("civic").onclick = function() {
+    window.location.href = "civic.html";
+}
+
+//geography
+document.getElementById("geo").onclick = function() {
+    window.location.href = "geography.html";
+}
+
+//tamil
+document.getElementById("tam").onclick = function() {
+    window.location.href = "tamil.html";
+}
+
+//music
+document.getElementById("mu").onclick = function() {
+    window.location.href = "music.html";
+}
+
+//dance
+document.getElementById("dan").onclick = function() {
+    window.location.href = "dance.html";
+}
+
+//ict
+document.getElementById("ict").onclick = function() {
+    window.location.href = "ict.html";
+}
+
+//helth
+document.getElementById("hel").onclick = function() {
+    window.location.href = "helth.html";
+}
+
+//agri science
+document.getElementById("agri").onclick = function() {
+    window.location.href = "agri.html";
+}
